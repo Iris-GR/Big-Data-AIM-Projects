11975709772a8d33d64fff4e1aacf16e5c449803
Iris-GR <rodal@student.chalmers.se>
2022-03-24 14:09:43 +0100

    Add the pre-written loading of data set

--- MVE441_P1_Iris.R
+++ MVE441_P1_Iris.R
@@ -1,4 +1,46 @@
 # MVE441, Project 1, (Iris)
 
 # Load libraries 
+library(readr)
+library(tibble)
+library(dplyr)
+library(purrr)
 
+# Factor names
+names <- c("id_number", "diagnosis", "radius_mean",
+           "texture_mean", "perimeter_mean", "area_mean",
+           "smoothness_mean", "compactness_mean",
+           "concavity_mean","concave_points_mean",
+           "symmetry_mean", "fractal_dimension_mean",
+           "radius_se", "texture_se", "perimeter_se",
+           "area_se", "smoothness_se", "compactness_se",
+           "concavity_se", "concave_points_se",
+           "symmetry_se", "fractal_dimension_se",
+           "radius_worst", "texture_worst",
+           "perimeter_worst", "area_worst",
+           "smoothness_worst", "compactness_worst",
+           "concavity_worst", "concave_points_worst",
+           "symmetry_worst", "fractal_dimension_worst")
+
+# Load the breast cancer data set
+uci_bc_data <- read_delim(
+  "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data",
+  delim = ",",
+  col_names = names,
+  col_types = cols(
+    .default = col_number(),
+    id_number = col_integer(),
+    diagnosis = col_factor()
+  ))
+
+# Define the response variable y 
+y <- uci_bc_data %>% 
+  mutate(diagnosis = as.factor(case_when(diagnosis == "B" ~ 0, diagnosis == "M" ~ 1))) %>%
+  select(diagnosis) %>%
+  as_vector() %>%
+  unname()
+
+# Define the predictors
+X <- uci_bc_data %>% 
+  select(-id_number, -diagnosis) %>%
+  as.matrix()
