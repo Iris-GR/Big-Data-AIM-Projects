ab0be380509dd75a04cf7f2cadffc8b09d75b218
iAreBaboon <tomas.pousette@gmail.com>
2022-03-28 09:43:29 +0200

    added code for training CART-classifier on the data

--- MVE441_P1_Tomas.R
+++ MVE441_P1_Tomas.R
@@ -5,6 +5,8 @@ library(purrr)
 library(caret)
 library(tidyverse)
 library(latex2exp)
+library(rpart)
+library(rpart.plot)
 
 #Generating data set for CART:
 X<-NULL
@@ -33,8 +35,18 @@ for (i in 1:100) {
   X = rbind(X, c(x,y,class))
 }
 
-
+#Plot the generated data
 dfX <- as.data.frame(X)
 attach(dfX); plot(V1, V2, col=c("red","blue")[V3+1]); detach(dfX)
 
+#Train CART and plot the decision tree
+dfX$V3 <- as.factor(dfX$V3)
+z = rpart(V3 ~ ., data = dfX, method = "class", cp =0, minsplit = 2, minbucket = 0)
+prp(z, type = 1, extra = 1, under = TRUE)
+
+#Try splitting data into train and test and see how well it works
+
+#Try training QDA-classifier and see how well it works
+
+#Generalize to be able to try on many different datasets
 
