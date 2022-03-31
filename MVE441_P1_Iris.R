45137b3372c0dafbe3ba17deeb63172ae97af497
Iris-GR <rodal@student.chalmers.se>
2022-03-31 13:18:16 +0200

    Updated question 3

--- MVE441_P1_Iris.R
+++ MVE441_P1_Iris.R
@@ -9,6 +9,10 @@ library(tibble)
 library(dplyr)
 library(purrr)
 library(corrplot)
+library(e1071)
+library(randomForest)
+library(ranger)
+library(pROC)
 #library(tidyverse)
 
 #### Question 1 ####
@@ -401,19 +405,22 @@ data <- data[,-1]
 # Dataset with 0-1 converted diagnosis, without id number, and features with 
 # high correlation are removed (X_high_cor_filtered - calculated in previous 
 # section). Very ugly, need to improve...
-# data_cor_filtered <- data %>% select(-(c(colnames(X_high_cor_filtered)))) 
+# data <- data %>% select(-(c(colnames(X_high_cor_filtered)))) 
+# Tuning parameters seem a bit worse for data with correlated variables removed
 
-
-## Split into training and testing data (random sampling WITHOUT stratification)
+### Split into training and testing data (random sampling WITHOUT stratification)
 set.seed(122)
 in_training <- sample(1:dim(data)[1],0.75*dim(data)[1], replace = FALSE)
 data_train <- data[ in_training,]
 data_test  <- data[-in_training,]
 
 
-# Specify the type of resampling 
-fitControl <- trainControl(method = "cv",
-                           number = 10)
+# Specify the type of sampling 
+# fitControl <- trainControl(method = "cv",
+#                            number = 10)
+fitControl <- trainControl(method = "repeatedcv",
+                           number = 10,
+                           repeats = 5)
 
 #QDA----------------------------------------------------------------------------
 # Fit the training data for QDA model for accuracy
@@ -438,29 +445,221 @@ CM_qda
 
 # ROC curve---------------------------------------------------------------------
 
+#.....
+
 # Random Forest-----------------------------------------------------------------
+# There are very many different types of random forests...
+# Fit the training data for random forest model for accuracy
+rf_fit_data <- train(diagnosis ~ .,
+                      data = data_train,
+                      method = 'ranger',
+                      trControl = fitControl,
+                      preProcess = c("center", "scale")) # could add corr correction already here
+
+rf_fit_data
+# Use QDA model fitted on training data to predict class of testing data
+rf_model_test_data <- as.factor(predict(rf_fit_data,
+                                         newdata = data_test))
+
+
+# Confusion matrix for QDA model fitted on training data to predict class of
+# testing data (data = predicted by model, reference = observed/true)
+CM_rf <-confusionMatrix(data = rf_model_test_data,
+                         reference = as.factor(data_test$diagnosis))
+CM_rf
 
 # Add selcetion function... ?best
 
 #-------------------------------------------------------------------------------
 
 
-
-## Split into training and testing data (WITH stratification)
+### Split into training and testing data (WITH stratification)
 set.seed(123)
 in_training_strat <- createDataPartition(data$diagnosis, p = .75, list = FALSE)
 data_train_strat <- data[ in_training_strat,]
 data_test_strat  <- data[-in_training_strat,]
 
 
+# Specify the type of sampling 
+# fitControl_strat <- trainControl(method = "cv",
+#                                 number = 10)
+fitControl_strat <- trainControl(method = "repeatedcv",
+                           number = 10,
+                           repeats = 5)
 
-#### Question 4 b) ####
+#QDA----------------------------------------------------------------------------
+# Fit the training data for QDA model for accuracy
+qda_fit_data_strat <- train(diagnosis ~ .,
+                      data = data_train_strat,
+                      method = "qda",
+                      metric = "Accuracy",
+                      trControl = fitControl_strat,
+                      preProcess = c("center", "scale")) # could add corr correction already here
+
+qda_fit_data_strat
+# Use QDA model fitted on training data to predict class of testing data
+qda_model_test_data_strat <- as.factor(predict(qda_fit_data_strat,
+                                         newdata = data_test_strat))
 
 
+# Confusion matrix for QDA model fitted on training data to predict class of
+# testing data (data = predicted by model, reference = observed/true)
+CM_qda_strat <-confusionMatrix(data = qda_model_test_data_strat,
+                         reference = as.factor(data_test_strat$diagnosis))
+CM_qda_strat
+# Random Forest-----------------------------------------------------------------
+# There are very many different types of random forests...
+# Fit the training data for random forest model for accuracy
+rf_fit_data_strat <- train(diagnosis ~ .,
+                     data = data_train_strat,
+                     method = 'ranger', # There is also 'rf'
+                     trControl = fitControl_strat,
+                     preProcess = c("center", "scale")) # could add corr correction already here
+
+rf_fit_data_strat
+# Use QDA model fitted on training data to predict class of testing data
+rf_model_test_data_strat <- as.factor(predict(rf_fit_data_strat,
+                                        newdata = data_test_strat))
 
 
+# Confusion matrix for QDA model fitted on training data to predict class of
+# testing data (data = predicted by model, reference = observed/true)
+CM_rf_strat <-confusionMatrix(data = rf_model_test_data_strat,
+                        reference = as.factor(data_test_strat$diagnosis))
+CM_rf_strat
+
+#-------------------------------------------------------------------------------
 
 
+#### Question 4 b) 
+
+# Create dataset starting fresh from the uci_bc_data. Change diagnosis to 0-1
+# factor and remove Id number
+data <- mutate(uci_bc_data, diagnosis = as.factor(case_when(diagnosis == "B" ~ 0, diagnosis == "M" ~ 1)))
+data <- data[,-1]
+
+# Sample two objects from the majority class at random
+# Create a new X-feature by taking a weighted average of the two X-samples with 
+# weights .95 and .05 for example, the point being that it should be a feature 
+# close but not identical to an original sample.
+
+# Generate imbalanced data out of the breast cancer dataset
+set.seed(124)
+
+# Filter out the data with class '0' as that is the majority class
+data_class_0 <- data %>% filter(diagnosis == '0')
+
+# Convert diagnosis from factor to numeric to be able to multiply with weight
+data_class_0$diagnosis <- as.numeric(as.character(data_class_0$diagnosis))
+
+# Sample two objects (two rows) from the data_class_0 and create new row
+# Clear New_rows 
+nr_new_rows <- 100
+New_rows = NULL
+for (i in 1:nr_new_rows) {
+  set.seed(i)
+  row.num <- sample(1:dim(data_class_0)[1], 2, replace = FALSE)
+  new_2_rows <- data_class_0[row.num,] * c(0.95, 0.05, nrow(2))
+  new_row <- new_2_rows[1,]+ new_2_rows[2,]
+  New_rows <- rbind(new_row, New_rows)
+}
+
+# Convert back diagnosis from numeric to factor in the New rows
+New_rows$diagnosis <- as.factor(as.numeric(New_rows$diagnosis))
+
+# Construct a more imbalanced dataset by combining data and New_rows
+imbal_data <- rbind(data, New_rows)
+
+
+### Split imbalanced data into training and testing data (WITH stratification)
+set.seed(125)
+in_imbal_train <- createDataPartition(imbal_data$diagnosis, 
+                                      p = .75, 
+                                      list = FALSE)
+imbal_train <- data[ in_imbal_train,]
+imbal_test <- data[-in_imbal_train,]
+
+# Down-sampling
+down_train <- downSample(x = imbal_train[, -ncol(imbal_train)],
+                         y = imbal_train$diagnosis)
+table(down_train$diagnosis) 
+
+# Up-sampling
+up_train <- upSample(x = imbal_train[, -ncol(imbal_train)],
+                     y = imbal_train$diagnosis)                         
+table(up_train$diagnosis) 
+
+# SMOTE
+smote_train <- SMOTE(diagnosis ~ ., data  = imbal_train)                         
+table(smote_train$diagnosis)
+
+# ROSE
+rose_train <- ROSE(diagnosis ~ ., data  = imbal_train)$data                         
+table(rose_train$diagnosis) 
+
+
+trl <- trainControl(method = "repeatedcv", repeats = 5,
+                    classProbs = TRUE,
+                    summaryFunction = twoClassSummary)
+
+# Bagged classification and estimate the area under the ROC curve using five 
+# repeats of 10-fold CV (straight from the documentation example so try out 
+# other methods also...) 
+set.seed(127)
+orig_fit <- train(Class ~ ., data = imbal_train, 
+                  method = "treebag",
+                  nbagg = 50,
+                  metric = "ROC",
+                  trControl = ctrl)
+
+down_outside <- train(Class ~ ., data = down_train, 
+                      method = "treebag",
+                      nbagg = 50,
+                      metric = "ROC",
+                      trControl = ctrl)
+
+up_outside <- train(Class ~ ., data = up_train, 
+                    method = "treebag",
+                    nbagg = 50,
+                    metric = "ROC",
+                    trControl = ctrl)
+
+rose_outside <- train(Class ~ ., data = rose_train, 
+                      method = "treebag",
+                      nbagg = 50,
+                      metric = "ROC",
+                      trControl = ctrl)
+
+
+smote_outside <- train(Class ~ ., data = smote_train, 
+                       method = "treebag",
+                       nbagg = 50,
+                       metric = "ROC",
+                       trControl = ctrl)
+
+outside_models <- list(original = orig_fit,
+                       down = down_outside,
+                       up = up_outside,
+                       SMOTE = smote_outside,
+                       ROSE = rose_outside)
+
+# Compare methods and summarize
+# outside_resampling <- resamples(outside_models)
+# 
+# test_roc <- function(model, data) {
+#   roc_obj <- roc(data$Class, 
+#                  predict(model, data, type = "prob")[, "Class1"],
+#                  levels = c("Class2", "Class1"))
+#   ci(roc_obj)
+# }
+# 
+# outside_test <- lapply(outside_models, test_roc, data = imbal_test)
+# outside_test <- lapply(outside_test, as.vector)
+# outside_test <- do.call("rbind", outside_test)
+# colnames(outside_test) <- c("lower", "ROC", "upper")
+# outside_test <- as.data.frame(outside_test)
+# 
+# summary(outside_resampling, metric = "ROC")
 
 
 
